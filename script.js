2be2925a5d837958000dcb4e570987c3ab9bd5ab
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-11-21 17:48:06 +0200

    form changed to buttons

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const sketchBoard = document.querySelector(".sketch-board");
-const eraseButton = document.querySelector(".erase-button")
+
+const eraseButton = document.querySelector(".eraser-button");
+const colorButtons = document.querySelectorAll(".color-button");
+const newGridButton = document.querySelector(".new-grid-button");
+
+
+
+
+
+let color = '#000000';
+let gridPixelSize = 16;
 
 function createGrid(pixelSize) {
+    grid(pixelSize)
+
+    let gridPixels = sketchBoard.querySelectorAll('div');
+
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', pixelColor));
+}
+
+function grid(pixelSize) {
     let gridArea = pixelSize * pixelSize;
+
     for(let i = 0; i < gridArea; i++) {
         let gridCell = document.createElement('div');
         gridCell.classList.add("pixel")
@@ -10,53 +29,76 @@ function createGrid(pixelSize) {
         sketchBoard.style.gridTemplateRows = `repeat(${pixelSize}, auto)`;
         sketchBoard.insertAdjacentElement('beforeend', gridCell);
     }
-    let gridPixels = sketchBoard.querySelectorAll('div');
-    eraseButton.addEventListener('click', erase);
-    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', draw));
 }
 
-
-function draw() {
-    this.style.backgroundColor = '#000000';
+function pixelColor() {
+    this.style.backgroundColor = color;
 }
 
 function drawRedShade() {
     let red = 255;
-    let green = Math.floor(Math.random()*140);
+    let green = Math.floor(Math.random()*160);
     let blue = Math.floor(Math.random()*150);
-    this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    color = `rgb(${red}, ${green}, ${blue})`;
 }
 
 function drawBlueShade() {
-    let red = 100;
-    let green = Math.floor(Math.random()*255);
-    let blue = 255;
-    this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
-
-function drawRandomColor() {
     let red = Math.floor(Math.random()*255);
     let green = Math.floor(Math.random()*255);
-    let blue = Math.floor(Math.random()*255);
-    let color = 
-    this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
-
-function drawWhite(){
-    this.style.backgroundColor = '#FFFFFF';
+    let blue = 255;
+    color = `rgb(${red}, ${green}, ${blue})`;
 }
 
 //For erase button
-function erase() {
-    gridPixel.style.backgroundColor = white
+function eraseAllColor() {
+    let gridPixels = sketchBoard.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = '#ffffff');
 }
 
-function radioSelected() {
-    let blackPen = document.querySelector("#black");
+function playersPickedColor(c) {
     
+console.log(c)
+}
+
+let theInput = document.getElementById("input-color");
+
+theInput.addEventListener('onchange', playersPickedColor(this))
+
+
+function colorPicker(event) {
+    switch(event.target.dataset.color) {
+        case 'red':
+            drawRedShade();
+            break;
+        case 'blue':
+            drawBlueShade();
+            break;            
+        case 'black':
+            color = 'black';
+            break;   
+    }
 }
 
 
-createGrid(15);
+
+
+function pixelSize() {
+    const displaySize = document.querySelector('.range-number');
+    const sizeSlider = document.getElementById("my-range");
+    sizeSlider.value = 16;
+    displaySize.textContent = sizeSlider.value + ' pixels';
+    sizeSlider.addEventListener('mousemove', function() {
+        displaySize.textContent = sizeSlider.value + ' pixels';
+    })
+}
+
+
+pixelSize();
+
+
+createGrid(gridPixelSize);
+
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', colorPicker));
+eraseButton.addEventListener('click', eraseAllColor);
 
 
